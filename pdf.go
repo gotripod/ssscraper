7053eeb21655e0e7b8aa1d41426bd7ae7ed6a76e
Colin Ramsay <info@colinramsay.co.uk>
2024-02-01 16:40:12 +0000

    fix: PDF title

--- pdf.go
+++ pdf.go
@@ -32,19 +32,20 @@ func ConvertPDFText(path string) (BodyResult, MetaResult, error) {
 		// Parse meta output
 		for _, line := range strings.Split(string(metaStr), "\n") {
 			if parts := strings.SplitN(line, ":", 2); len(parts) > 1 {
-				metaResult.meta[strings.TrimSpace(parts[0])] = strings.TrimSpace(parts[1])
+				metaResult.meta[strings.ToLower(strings.TrimSpace(parts[0]))] = strings.TrimSpace(parts[1])
 			}
 		}
 
 		// Convert parsed meta
+
 		if x, ok := metaResult.meta["ModDate"]; ok {
 			if t, ok := pdfTimeLayouts.Parse(x); ok {
-				metaResult.meta["ModifiedDate"] = fmt.Sprintf("%d", t.Unix())
+				metaResult.meta["modifiedDate"] = fmt.Sprintf("%d", t.Unix())
 			}
 		}
 		if x, ok := metaResult.meta["CreationDate"]; ok {
 			if t, ok := pdfTimeLayouts.Parse(x); ok {
-				metaResult.meta["CreatedDate"] = fmt.Sprintf("%d", t.Unix())
+				metaResult.meta["createdDate"] = fmt.Sprintf("%d", t.Unix())
 			}
 		}
 
